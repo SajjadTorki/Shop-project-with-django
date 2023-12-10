ec9b6d4baa16c4b53eeb2859d1d909e677a8b128
SajjadTorki <sajjadtorki72@gmail.com>
2023-12-10 17:16:57 +0330

    account and pages new testing

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+
 
 # Create your tests here.
+
+
+class TestAccount(TestCase):
+    def test_login_url(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_template_name(self):
+        response = self.client.get(reverse('account_login'))
+        self.assertEqual(response.status_code, 200)
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+
 
 # Create your tests here.
+
+
+class PageTest(TestCase):
+    def test_pages_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_pages_template_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, "<h1>Home Page</h1>")
+        self.assertEqual(response.status_code, 200)
