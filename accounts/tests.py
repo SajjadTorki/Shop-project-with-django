f81cd55f96db99a4a421cb8be5fdc247409a3802
SajjadTorki <sajjadtorki72@gmail.com>
2023-12-11 13:56:50 +0330

    Add static Files

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from .models import CustomUser
 
 
 # Create your tests here.
 
 
 class TestAccount(TestCase):
+
     def test_login_url(self):
         response = self.client.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
