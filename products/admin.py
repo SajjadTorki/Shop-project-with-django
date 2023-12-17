150c5b78c2ad6f461f6fcda47d706470d5c53813
SajjadTorki <sajjadtorki72@gmail.com>
2023-12-17 22:19:05 +0330

    Product and comment add new feature

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from .models import Product
+from .models import Product, Comment
 
 
 # Register your models here.
@@ -9,3 +9,8 @@ from .models import Product
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'product', 'active', 'datetime_created', 'star']
--- products/models.py
+++ products/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 # Create your models here.
 
 
 class Product(models.Model):
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
@@ -18,3 +20,26 @@ class Product(models.Model):
 
     def get_absolut_url(self):
         return reverse("product_detail", args=[self.pk])
+
+
+class Comment(models.Model):
+    STAR_CHOICES = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'very good'),
+    ]
+    author = models.ForeignKey(get_user_model(), related_name='comments', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    star = models.CharField(max_length=10, choices=STAR_CHOICES)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Comment : {self.author}  , product :{self.product}'
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
