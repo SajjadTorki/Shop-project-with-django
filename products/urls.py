092b84af855969f265193056ce8b967fb237e6be
SajjadTorki <sajjadtorki72@gmail.com>
2023-12-18 13:56:18 +0330

    Developing for view comment , send comment

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
+    path('comment/<int:pk>/', CommentView.as_view(), name="comment_created"),
 ]
