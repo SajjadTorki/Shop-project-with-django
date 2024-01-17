63810e91381b6d8af515ea206cb4578f2c6bfb4e
SajjadTorki <sajjadtorki72@gmail.com>
2024-01-17 11:01:06 +0330

    Context&Functionallt

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Custom Context Processor
+                'cart.context_processors.cart'
             ],
         },
     },
@@ -152,10 +154,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# Statics
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-
+# Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- products/models.py
+++ products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product/product_cover/', verbose_name=_("Product Image"), blank=True, )
     active = models.BooleanField(default=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
