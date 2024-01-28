6f8c8a43aef700f12f198ccc01b81ad0f3e54cbc
SajjadTorki <sajjadtorki72@gmail.com>
2024-01-28 07:50:32 +0330

    Create About Us

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about_Us/', views.about_us_view, name='about_us'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+def about_us_view(request):
+    return render(request, 'about_us.html')
